29bc9800685f17ccb79de96768cdfe3d8f70d016
Luidy Teles Araujo <luidyteles620@gmail.com>
2025-02-25 22:49:59 -0300

    Feat - Pontos finais

--- mail.php
+++ mail.php
@@ -1,31 +1,37 @@
 <?php
-    $to = 'demo@site.com';
-    $firstname = $_POST["fname"];
-    $email= $_POST["email"];
-    $text= $_POST["message"];
-    $phone= $_POST["phone"];
-    
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Capturando os dados do formulário
+    $to = 'demo@site.com'; // Altere para seu email real
+    $firstname = isset($_POST["name"]) ? $_POST["name"] : '';
+    $email = isset($_POST["email"]) ? $_POST["email"] : '';
+    $subject = isset($_POST["subject"]) ? $_POST["subject"] : 'Sem assunto';
+    $text = isset($_POST["message"]) ? $_POST["message"] : '';
 
-
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    $message ='<table style="width:100%">
-        <tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
-        </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$phone.'</td></tr>
-        <tr><td>Text: '.$text.'</td></tr>
-        
-    </table>';
-
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'The message has been sent.';
-    }else{
-        echo 'failed';
+    // Validação básica (verifica se os campos obrigatórios estão preenchidos)
+    if (empty($firstname) || empty($email) || empty($subject) || empty($text)) {
+        die("Erro: Todos os campos são obrigatórios.");
     }
 
+    // Cabeçalhos do e-mail
+    $headers = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+    $headers .= "From: $email\r\n";
+
+    // Corpo do e-mail
+    $message = '<table style="width:100%; border-collapse: collapse;" border="1">
+        <tr><td><strong>Nome:</strong></td><td>' . htmlspecialchars($firstname) . '</td></tr>
+        <tr><td><strong>Email:</strong></td><td>' . htmlspecialchars($email) . '</td></tr>
+        <tr><td><strong>Assunto:</strong></td><td>' . htmlspecialchars($subject) . '</td></tr>
+        <tr><td><strong>Mensagem:</strong></td><td>' . nl2br(htmlspecialchars($text)) . '</td></tr>
+    </table>';
+
+    // Envia o e-mail
+    if (mail($to, $subject, $message, $headers)) {
+        echo 'A mensagem foi enviada com sucesso!';
+    } else {
+        echo 'Falha ao enviar a mensagem.';
+    }
+} else {
+    echo 'Método de requisição inválido.';
+}
 ?>
